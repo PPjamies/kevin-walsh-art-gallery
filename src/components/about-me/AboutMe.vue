06bbee0049f18b49700f18028b79a62b595ea18a
PJ <pazuzujindrich@gmail.com>
2024-09-19 14:08:54 -0700

    added css variables and themes. created an about me page with sticky image.

---
+++ src/components/about-me/AboutMe.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+  props: {
+    alt: String,
+    url: String,
+    title: String,
+    description: String
+  }
+}
+</script>
+
+<template>
+  <div class="about-me-container">
+    <img class="sticky-image profile-image-normal"
+         :alt="alt"
+         :src="url"/>
+    <div class="info-section">
+      <h1><strong>{{ this.title }}</strong></h1>
+      <p>{{ this.description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.about-me-container {
+  display: flex;
+}
+
+.sticky-image {
+  position: sticky;
+  top: 20px;
+}
+
+.info-section {
+  padding: var(--p-2);
+  text-align: var(--text-left);
+}
+
+.info-section p {
+  font-size: var(--text-base);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import {createApp} from "vue";
 import PrimeVue from "primevue/config";
 import App from "@/App.vue";
 
+/* styling */
+import './assets/variables.css';
+import './assets/themes.css';
+
 const app = createApp(App);
 
 app.use(PrimeVue);
